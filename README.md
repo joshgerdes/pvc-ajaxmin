6141af6d838adf824b022e75e600af10bb0c6ba3
Josh Gerdes <joshgerdes@gmail.com>
2014-05-23 16:33:05 -0500

    adding base functional plugin

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
+![PVC Build Engine](http://i.imgur.com/vyROdJJ.png)
+
 pvc-ajaxmin
 ===========
+
+This is a plugin to utilize the Microsoft Ajax Minifier with the [PVC Build Engine](https://github.com/pvcbuild).
---
+++ src/PvcAjaxmin.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.Ajax.Utilities;
+using PvcCore;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text.RegularExpressions;
+
+namespace PvcPlugins
+{
+    public class PvcAjaxmin : PvcPlugin
+    {
+        public override IEnumerable<PvcCore.PvcStream> Execute(IEnumerable<PvcCore.PvcStream> inputStreams)
+        {
+            var minifyStreams = inputStreams.Where(x => Regex.IsMatch(x.StreamName, @"\.(js|css)$"));
+            var resultStreams = new List<PvcStream>();
+
+            foreach (var inputStream in minifyStreams)
+            {
+                var fileContent = new StreamReader(inputStream).ReadToEnd();
+                var minifier = new Minifier();
+                var resultContent = inputStream.StreamName.EndsWith(".js") ? minifier.MinifyJavaScript(fileContent) : minifier.MinifyStyleSheet(fileContent);
+
+                foreach (var error in minifier.ErrorList)
+                {
+                    Console.Error.WriteLine(error.ToString());
+                }
+
+                var dirName = Path.GetDirectoryName(inputStream.StreamName);
+                var fileName = Path.GetFileNameWithoutExtension(inputStream.StreamName) + ".min" + Path.GetExtension(inputStream.StreamName);
+
+                var resultStream = PvcUtil.StringToStream(resultContent, Path.Combine(dirName, fileName));
+                resultStreams.Add(resultStream);
+            }
+
+            return inputStreams.Where(x => !minifyStreams.Any(y => y.StreamName == x.StreamName)).Concat(resultStreams);
+        }
+    }
+}
