4e91b6e00c8ad872294163060af4e6706073d803
Josh Gerdes <joshgerdes@gmail.com>
2014-05-25 21:23:25 -0500

    Updated to use SupportedTags filter from latest PVC. Added command line switch parameter support.

--- README.md
+++ README.md
@@ -4,3 +4,25 @@ pvc-ajaxmin
 ===========
 
 This is a plugin to utilize the Microsoft Ajax Minifier with the [PVC Build Engine](https://github.com/pvcbuild).
+
+###Parameter Options
+
+The plugin has the ability to take in command line switches for the Microsoft Ajax Minifier. More details on what can be passed in can be found [here](http://ajaxmin.codeplex.com/wikipage?title=Command-Line%20Switches).
+
+###Usage Examples
+
+Basic:
+
+```
+pvc.Source("js/*", "css/*", "test.js", "test.css")
+	.Pipe(new PvcAjaxmin())
+	.Save(@"deploy");
+```
+
+With Parameters:
+
+```
+pvc.Source("css/*")
+	.Pipe(new PvcAjaxmin("-colors:hex"))
+	.Save(@"deploy");
+```
--- src/PvcAjaxmin.cs
+++ src/PvcAjaxmin.cs
@@ -10,30 +10,62 @@ namespace PvcPlugins
 {
     public class PvcAjaxmin : PvcPlugin
     {
+        private string commandLineSwitches;
+
+        public PvcAjaxmin(string commandLineSwitches = "") 
+        {
+            this.commandLineSwitches = commandLineSwitches;
+        }
+
+        public override string[] SupportedTags
+        {
+            get
+            {
+                return new[] { ".css", ".js" };
+            }
+        }
+
         public override IEnumerable<PvcCore.PvcStream> Execute(IEnumerable<PvcCore.PvcStream> inputStreams)
         {
-            var minifyStreams = inputStreams.Where(x => Regex.IsMatch(x.StreamName, @"\.(js|css)$"));
             var resultStreams = new List<PvcStream>();
+            SwitchParser switchParser = null;
 
-            foreach (var inputStream in minifyStreams)
+            if (!String.IsNullOrEmpty(this.commandLineSwitches))
+            {
+                switchParser = new SwitchParser();
+                switchParser.Parse(this.commandLineSwitches);
+            }
+
+            foreach (var inputStream in inputStreams)
             {
                 var fileContent = new StreamReader(inputStream).ReadToEnd();
                 var minifier = new Minifier();
-                var resultContent = inputStream.StreamName.EndsWith(".js") ? minifier.MinifyJavaScript(fileContent) : minifier.MinifyStyleSheet(fileContent);
+                string resultContent = null;
+
+                if (inputStream.StreamName.EndsWith(".js"))
+                {
+                    resultContent = (switchParser != null) ? minifier.MinifyJavaScript(fileContent, switchParser.JSSettings) : minifier.MinifyJavaScript(fileContent);
+                }
+                else 
+                {
+                    resultContent = (switchParser != null) ? minifier.MinifyStyleSheet(fileContent, switchParser.CssSettings) : minifier.MinifyStyleSheet(fileContent);              
+                }
 
                 foreach (var error in minifier.ErrorList)
                 {
                     Console.Error.WriteLine(error.ToString());
                 }
 
-                var dirName = Path.GetDirectoryName(inputStream.StreamName);
-                var fileName = Path.GetFileNameWithoutExtension(inputStream.StreamName) + ".min" + Path.GetExtension(inputStream.StreamName);
-
-                var resultStream = PvcUtil.StringToStream(resultContent, Path.Combine(dirName, fileName));
-                resultStreams.Add(resultStream);
+                if (resultContent != null)
+                {
+                    var dirName = Path.GetDirectoryName(inputStream.StreamName);
+                    var fileName = Path.GetFileNameWithoutExtension(inputStream.StreamName) + ".min" + Path.GetExtension(inputStream.StreamName);
+                    var resultStream = PvcUtil.StringToStream(resultContent, Path.Combine(dirName, fileName));
+                    resultStreams.Add(resultStream);
+                }
             }
 
-            return inputStreams.Where(x => !minifyStreams.Any(y => y.StreamName == x.StreamName)).Concat(resultStreams);
+            return resultStreams;
         }
     }
 }
